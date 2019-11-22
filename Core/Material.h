fa6d723f2904ce3ebd8b9eafce28c7f07b5e0137
Lion Kortlepel <development@kortlepel.com>
2019-11-22 16:40:38 +0100

    Add Material

--- Core/Material.h
+++ Core/Material.h
@@ -1,11 +1,31 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 
+#include "Global.h"
+#include "Texture.h"
 
 class Material
 {
 public:
     Material();
+
+    inline bool has_texture() const { return m_texture.is_not_null(); }
+    inline void set_texture(RefPtr<Texture>&& tex) { m_texture = std::move(tex); }
+    
+    inline RefPtr<Texture> texture()
+    {
+        ASSERT(m_texture.is_not_null());
+        return m_texture;
+    }
+    
+    inline const RefPtr<Texture> texture() const
+    {
+        ASSERT(m_texture.is_not_null());
+        return m_texture;
+    }
+
+protected:
+    RefPtr<Texture> m_texture;
 };
 
 #endif // MATERIAL_H
