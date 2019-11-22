8196bee1513e04ba716e01b18197612a4c4376a1
Lion Kortlepel <development@kortlepel.com>
2019-11-22 16:43:32 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "Core/Global.h"
+#include "Core/Material.h"
 
 int main(int, char**)
 {
-    
+    Material mat;
+    auto x = mat.texture();
+    std::cout << "Hello, World!" << std::endl;
 }
 
