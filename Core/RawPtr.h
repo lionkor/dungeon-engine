28d6b97cef2ac9dcb2a512571959cc4ce0074018
Lion Kortlepel <development@kortlepel.com>
2019-11-22 19:17:27 +0100

    Core/RawPtr: Add operator* and operator->

--- Core/RawPtr.h
+++ Core/RawPtr.h
@@ -10,10 +10,17 @@ public:
     RawPtr(_T* data) : data(data) {}
     RawPtr(const RawPtr& p) : data(p.data) {}
     RawPtr(RawPtr&& p) : data(std::move(p.data)) {}
-    
+
     operator _T*() { return data; }
     operator const _T*() const { return data; }
     operator bool() { return data; }
+
+    _T& operator*() { return *data; }
+    const _T& operator*() const { return *data; }
+
+    _T* operator->() { return data; }
+    const _T* operator->() const { return data; }
+
 protected:
     _T* data = nullptr;
 };
