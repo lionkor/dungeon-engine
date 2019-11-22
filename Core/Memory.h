247ac51750acdc967647312bd556f2a9767d83ee
Lion Kortlepel <development@kortlepel.com>
2019-11-22 16:34:25 +0100

    Add RefPtr, OwnPtr, WeakPtr
    These are wrappers around <memory> smart pointers!
    This is done so that we can replace them easily if they perform too bad.

--- Core/Memory.h
+++ Core/Memory.h
@@ -8,7 +8,17 @@ template<typename _T>
 class WeakPtr : public std::weak_ptr<_T>
 {
 public:
+    template<typename... _Args>
+    WeakPtr(_Args&&... args)
+        : std::weak_ptr<_T>(args...)
+    {
+    }
+    
     inline RawPtr<_T> raw() { return this->get(); }
+
+    inline bool is_null() const { return !this->get(); }
+    inline bool is_not_null() const { return this->get(); }
+
     inline const RawPtr<_T> raw() const { return this->get(); }
 };
 
@@ -19,12 +29,21 @@ public:
     template<typename... _Args>
     static RefPtr make(_Args&&... args)
     {
-        return std::make_shared(std::forward(args)...);
+        return std::make_shared<_T>(args...);
     }
     
+    template<typename... _Args>
+    RefPtr(_Args&&... args)
+        : std::shared_ptr<_T>(args...)
+    {
+    }
+
+    inline bool is_null() const { return !this->get(); }
+    inline bool is_not_null() const { return this->get(); }
+
     inline RawPtr<_T> raw() { return this->get(); }
     inline const RawPtr<_T> raw() const { return this->get(); }
-    inline WeakPtr<_T> weak() { return WeakPtr<_T>(this); }
+    inline WeakPtr<_T> weak_ptr() { return WeakPtr<_T>(this); }
 };
 
 template<typename _T>
@@ -32,11 +51,20 @@ class OwnPtr : public std::unique_ptr<_T>
 {
 public:
     template<typename... _Args>
-    static OwnPtr make(_Args&&... args)
+    OwnPtr(_Args&&... args)
+        : std::unique_ptr<_T>(args...)
     {
-        return std::make_shared(std::forward(args)...);
     }
     
+    template<typename... _Args>
+    static OwnPtr&& make(_Args&&... args)
+    {
+        return std::move(OwnPtr(new _T(args...)));
+    }
+
+    inline bool is_null() const { return this->get(); }
+    inline bool is_not_null() const { return this->get(); }
+
     inline RawPtr<_T> raw() { return this->get(); }
     inline const RawPtr<_T> raw() const { return this->get(); }
 };
