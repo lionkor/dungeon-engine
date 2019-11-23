1995e43820d0d3aa02cdcc5811e5e757ab00f045
Lion Kortlepel <development@kortlepel.com>
2019-11-23 15:45:27 +0100

    Start working on serialization for save/load

--- Core/Serializable.h
+++ Core/Serializable.h
@@ -1,4 +1,14 @@
 #ifndef SERIALIZABLE_H
 #define SERIALIZABLE_H
 
+#include "Global.h"
+#include "Path.h"
+
+/// Interface for serialization
+class Serializable
+{
+public:
+    
+};
+
 #endif // SERIALIZABLE_H
