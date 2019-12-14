bdae2f8cde99332548c5f14b63b93aee22017dac
Lion Kortlepel <development@kortlepel.com>
2019-12-14 12:58:44 +0100

    make.sh: Build and symlink needed things! Yay!

--- make.sh
+++ make.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+mkdir -p bin include lib
+
+if [ ! -d String ]; then
+    echo "String directory not found. Make sure you cloned the repo with --recursive"
+    return 1
+fi
+
+if [ ! -f include/String ]; then
+    cd String
+    mkdir -p bin
+    cd bin
+    cmake -S ..
+    make
+    cd ../..
+    ln -s ${PWD}/String/include include/String
+    ln -s ${PWD}/String/bin/libString.a lib/libString.a
+fi
+
 cd bin
 rm -v dungeon-engine
 cmake -DCMAKE_BUILD_TYPE=Debug -S ..
