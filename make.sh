47019a57b8154757bba39bd383d80855c172b829
Lion Kortlepel <development@kortlepel.com>
2019-12-17 10:53:06 +0100

    make.sh: Build SFML

--- make.sh
+++ make.sh
@@ -7,6 +7,20 @@ if [ ! -d String ]; then
     return 1
 fi
 
+if [ ! -d SFML ]; then
+    echo "SFML directory not found. Make sure you cloned the repo with --recursive"
+    return 2
+fi
+
+if [ ! -s include/SFML ]; then
+    cd SFML
+    cmake . -DBUILD_SHARED_LIBS=false
+    make -j 9
+    cd ..
+    ln -s ${PWD}/SFML/lib/* lib/
+    ln -s ${PWD}/SFML/include/* include/
+fi
+
 if [ ! -s include/String ]; then
     cd String
     mkdir -p bin
