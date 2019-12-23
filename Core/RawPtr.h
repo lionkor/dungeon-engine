bd66047ecdd9e875a3608c39d960344b5e088ead
Lion Kortlepel <development@kortlepel.com>
2019-12-23 19:26:10 +0100

    Rename RawPtr::data to m_data

--- Core/RawPtr.h
+++ Core/RawPtr.h
@@ -7,36 +7,49 @@ template<typename _T>
 class RawPtr
 {
 public:
-    RawPtr(_T* data) : data(data) {}
-    RawPtr(const RawPtr& p) : data(p.data) {}
-    RawPtr(RawPtr&& p) : data(std::move(p.data)) {}
+    RawPtr() : m_data(nullptr) {}
+    RawPtr(_T* data) : m_data(data) {}
+    RawPtr(const RawPtr& p) : m_data(p.m_data) {}
+    // RawPtr(RawPtr&& p) : data(std::move(p.data)) {}
 
     // FIXME: add `as` method
 
-    operator _T*() { return data; }
-    operator const _T*() const { return data; }
-    operator bool() { return data; }
+    operator _T*() { return m_data; }
+    operator const _T*() const { return m_data; }
+    operator bool() { return m_data; }
 
     template<typename _TryT>
     _TryT& operator*()
     {
-        return *data;
+        return *m_data;
+    }
+
+    template<typename _TConvertType>
+    _TConvertType& as()
+    {
+        return *(_TConvertType*)(m_data);
+    }
+
+    template<typename _TConvertType>
+    _TConvertType* as_ptr()
+    {
+        return (_TConvertType*)(m_data);
     }
 
     template<typename _TryT>
     const _TryT& operator*() const
     {
-        return *data;
+        return *m_data;
     }
 
-    _T*       operator->() { return data; }
-    const _T* operator->() const { return data; }
+    _T*       operator->() { return m_data; }
+    const _T* operator->() const { return m_data; }
 
-    inline bool is_null() const { return !data; }
-    inline bool is_not_null() const { return data; }
+    inline bool is_null() const { return !m_data; }
+    inline bool is_not_null() const { return m_data; }
 
 protected:
-    _T* data = nullptr;
+    _T* m_data = nullptr;
 };
 
 #endif // RAWPTR_H
